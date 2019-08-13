03efd961ec61d75f3df2a9d38b93237f35f2fe66
Marcel Cunha <marcel.cunha.mc@gmail.com>
2019-08-13 15:22:57 -0300

    adição de provider discover

---
+++ src/Providers/LogServicePrivider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Dois\Log\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class LogServicePrivider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+}
